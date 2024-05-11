572e5631cdfd24ca1f211824028b547c93567deb
Fábio Demo da Rosa <faberdemo@gmail.com>
2024-05-11 17:50:21 +0000

    Formatted fact table

--- models/facts/fct_vendas.sql
+++ models/facts/fct_vendas.sql
@@ -1,23 +1,26 @@
-{{ config(materialized='incremental', unique_key='venda_id') }}
-WITH vendas AS (
-    SELECT
-        v.id_vendas AS venda_id,
-        v.id_veiculos AS veiculo_id,
-        v.id_concessionarias AS concessionaria_id,
-        v.id_vendedores AS vendedor_id,
-        v.id_clientes AS cliente_id,
-        v.valor_venda, 
-        v.data_venda,
-        v.data_inclusao,
-        v.data_atualizacao
-    FROM {{ ref('stg_vendas') }} v
-    JOIN {{ ref('dim_veiculos') }} vei ON v.id_veiculos = vei.veiculo_id
-    JOIN {{ ref('dim_concessionarias') }} con ON v.id_concessionarias = con.concessionaria_id
-    JOIN {{ ref('dim_vendedores') }} ven ON v.id_vendedores = ven.vendedor_id
-    JOIN {{ ref('dim_clientes') }} cli ON v.id_clientes = cli.cliente_id
-)
+{{ config(materialized="incremental", unique_key="venda_id") }}
+with
+    vendas as (
+        select
+            v.id_vendas as venda_id,
+            v.id_veiculos as veiculo_id,
+            v.id_concessionarias as concessionaria_id,
+            v.id_vendedores as vendedor_id,
+            v.id_clientes as cliente_id,
+            v.valor_venda,
+            v.data_venda,
+            v.data_inclusao,
+            v.data_atualizacao
+        from {{ ref("stg_vendas") }} v
+        join {{ ref("dim_veiculos") }} vei on v.id_veiculos = vei.veiculo_id
+        join
+            {{ ref("dim_concessionarias") }} con
+            on v.id_concessionarias = con.concessionaria_id
+        join {{ ref("dim_vendedores") }} ven on v.id_vendedores = ven.vendedor_id
+        join {{ ref("dim_clientes") }} cli on v.id_clientes = cli.cliente_id
+    )
 
-SELECT
+select
     venda_id,
     veiculo_id,
     concessionaria_id,
@@ -27,7 +30,7 @@ SELECT
     data_venda,
     data_inclusao,
     data_atualizacao
-FROM vendas
+from vendas
 {% if is_incremental() %}
-    WHERE venda_id > (SELECT MAX(venda_id) FROM {{ this }})
+    where venda_id > (select max(venda_id) from {{ this }})
 {% endif %}
